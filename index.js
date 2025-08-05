499528e846ae04aacc4574ca72603e8cc2ad5364
gamal sayed <gamalsayedccss@gmail.com>
2025-08-05 17:16:07 +0300

    edit cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config({
 const app = express();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL, // Replace with your frontend URL
+    // origin: process.env.CLIENT_URL, // Replace with your frontend URL
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, 
